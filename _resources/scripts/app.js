7b14869ef1b873cfdb04ebc5a632d73a2b820666
Oskar Schöldström <m@oxy.fi>
2019-12-29 19:38:44 -0300

    fix 100vh issues on mobile devices

--- _resources/scripts/app.js
+++ _resources/scripts/app.js
@@ -1,3 +1,11 @@
+const setGlobalVhValue = () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+setGlobalVhValue();
+window.addEventListener('resize', setGlobalVhValue);
+
 import './map';
 import './charts';
 import './commits';
